fe744a4c82cea1c9b2e9e0507915bac132db1e23
BrandtBre <breno.b1@unochapeco.edu.br>
2022-07-04 18:23:38 -0300

    backend api mto louco

---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'controle_financeiro_empresarial',
+    password: 'unochapeco'
+});
+
+module.exports = { query: (text, params) => pool.query(text, params) };
